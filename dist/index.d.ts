20bc1cba0a7128da7759306915b6892405d140db
Rem Lampa <rem@remlampa.com>
2021-04-24 19:43:03 +0800

    Add missing types for generateApolloClient

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,19 @@
+import type { ApolloClient } from '@apollo/client';
+import type { WebSocketLink } from 'apollo-link-ws';
+
+export function generateApolloClient(
+  auth?: any,
+  gqlEndpoint: string,
+  headers?: {
+    [key: string]: any;
+  },
+  publicRole?: string,
+  cache?: any,
+): {
+  client: ApolloClient;
+  wsLink: WebSocketLink | null;
+};
+
 export function NhostApolloProvider(
   auth: any,
   gqlEndpoint: string,
