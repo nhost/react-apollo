985564240793f10454778899c2a217c94996d3d3
elitan <johan@eliasson.me>
2021-06-12 15:10:11 +0200

    adding connectToDevTools

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { setContext } from "@apollo/client/link/context";
 
-export function generateApolloClient(
+export function generateApolloClient({
   auth,
   gqlEndpoint,
   headers,
   publicRole = "public",
-  cache
-) {
+  cache,
+  connectToDevTools = false,
+}) {
+  console.log("connect to dev tools");
+  console.log({ connectToDevTools });
+
   const getheaders = (auth) => {
     // add headers
     const resHeaders = {
@@ -93,6 +97,7 @@ export function generateApolloClient(
         fetchPolicy: "cache-and-network",
       },
     },
+    connectToDevTools,
   });
 
   return { client, wsLink };
@@ -108,14 +113,16 @@ export class NhostApolloProvider extends React.Component {
       headers,
       publicRole = "public",
       cache,
+      connectToDevTools,
     } = this.props;
-    const { client, wsLink } = generateApolloClient(
+    const { client, wsLink } = generateApolloClient({
       auth,
       gqlEndpoint,
       headers,
       publicRole,
-      cache
-    );
+      cache,
+      connectToDevTools,
+    });
     this.client = client;
     this.wsLink = wsLink;
 
